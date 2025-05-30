b28772513261c9876824511a0fb877e27125e5fa
BETINO-NDZE <150228775+BETINO-NDZE@users.noreply.github.com>
2025-05-30 06:30:08 +0100

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,5 +1,21 @@
-<?php 
+<?php
+// Use environment variables (recommended for Render)
+$host = getenv('dpg-d0sjoak9c44c73f7vevg-a');
+$dbname = getenv('news_portal_db_s0y9');
+$user = getenv('news_portal_db_s0y9_user');
+$password = getenv('xJgAnCJKCC3aF45xHfCsJn1UGedDoFb3');
+$port = getenv('5432');
 
-$connection = mysqli_connect('localhost','root','','live_news_project') or die("Not Connected". mysqli_error());
+// Default port for Postgres is 5432
+if (!$port) {
+    $port = 5432;
+}
 
+try {
+    $connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected to PostgreSQL!";
+} catch (PDOException $e) {
+    die("Not Connected: " . $e->getMessage());
+}
 ?>
